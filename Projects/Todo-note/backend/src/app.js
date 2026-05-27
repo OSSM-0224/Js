39647b8f8c4478cbc6062f29a8b719292dc25de7
Om Mhatre <108936280+OSSM-0224@users.noreply.github.com>
2026-05-27 17:27:03 +0530

    added get and update api for the todo lists

--- Projects/Todo-note/backend/src/app.js
+++ Projects/Todo-note/backend/src/app.js
@@ -4,7 +4,7 @@ import NoteModel from "./models/notes.model.js";
 const app = express();
 app.use(express.json());
 // @route POST /api/notes
-// @Create Create a new note need title and description in the request body
+// @description Create a new note need title and description in the request body
 // @access Public
 
 app.post("/api/notes", async (req, res) => {
@@ -26,7 +26,47 @@ app.post("/api/notes", async (req, res) => {
   }
 
   const newNote = await NoteModel.create({ title, description });
-   return res.json({ message: "Note Created sucessfully", newNote });
+  return res.json({ message: "Note Created sucessfully", newNote });
 });
+// @route GET /api/get-notes
+// @description Fetching all the notes
+// @access Public
+app.get("/api/get-notes", async (req, res) => {
+  const getNote = await NoteModel.find();
 
+  return res.status(200).json({
+    message: "Noted Fetched sucessfully",
+    getNote,
+  });
+});
+// @route PATCH /api/update-note/:noteId
+// @description updating a note by id require description in the request body
+// @access Public
+app.patch("/api/update-note/:id", async (req, res) => {
+  const { id } = req.params;
+  const { description } = req.body;
+  if (!description) {
+    return res
+      .status(400)
+      .json({ error: "Title AND Description are required" });
+  }
+  if (description.trim().length < 10) {
+    return res
+      .status(400)
+      .json({ error: "Description must be at least 10 characters long" });
+  }
+  const updateNote = await NoteModel.findById(id);
+
+  if (!updateNote) {
+    return res.status(404).json({ error: "Note not found" });
+  }
+
+  note.description = description;
+  await note.save();
+  
+  return res.status(200).json({
+    message: "Note updated Sucessfully",
+    updateNote,
+  });
+});
 export default app;
