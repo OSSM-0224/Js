898c44a7021cf01f01e2eaab920f54a658152d05
Om Mhatre <108936280+OSSM-0224@users.noreply.github.com>
2026-06-19 20:00:48 +0530

    fresh PRACTICE PROJECT

---
+++ Projects/BoudnaryLine/server.js
@@ -0,0 +1,11 @@
+import createApp from "./src/app.js";
+
+const app = createApp;
+
+const PORT = 3000;
+
+function startServer(){
+    app.listen(PORT, ()=>{
+        console.log(`Server is running on port ${PORT}`)
+    })
+}
